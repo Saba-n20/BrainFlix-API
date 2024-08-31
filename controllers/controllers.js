3747d016c20ebb4af604b14982856b64d8c5d2e1
saban <sabanamdari20@gmail.com>
2024-08-31 12:07:35 -0400

    Defining the getVideoById to get a video by id and some debugging line

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -9,3 +9,27 @@ export const getAllVideos = (req, res) => {
     res.status(500).send('Server error');
   }
 };
+
+// Get a specific video by ID
+export const getVideoById = (req, res) => {
+    try {
+      const videoId = req.params.id;
+      if (!videoId) {
+        return res.status(400).send('ID is required');
+      }
+      console.log('Received video ID:', videoId);
+      
+      const video = videos.find(v => v.id === videoId);
+      
+      if (video) {
+        res.status(200).json(video);
+      } else {
+        res.status(404).send('Video not found');
+      }
+      
+      console.log("Fetched video with ID:", videoId);
+    } catch (err) {
+      console.error('Server error:', err);
+      res.status(500).send('Server error');
+    }
+  };
