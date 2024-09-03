5ae21e85c61dc3dd3fab2f14b5dc8df5f2108d2c
saban <sabanamdari20@gmail.com>
2024-09-02 22:27:31 -0400

    Defining the function for adding new video

--- route/route.js
+++ route/route.js
@@ -3,11 +3,11 @@ import * as controller from '../controllers/controllers.js';
 
 const router = express.Router();
 
-router.route('/videos').get(controller.getAllVideos);
-router
-  .route('/videos/:id')
-  .get(controller.getVideoById)
-  // .post(controller.addVideoById)
-  
+router.route('/videos')
+  .get(controller.getAllVideos)
+  .post(controller.addVideoById);  // Ensure POST is correctly defined here
+
+router.route('/videos/:id')
+  .get(controller.getVideoById);
 
 export default router;
